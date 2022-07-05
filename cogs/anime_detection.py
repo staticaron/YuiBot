f8f09621ccfabdd86ca57d42104cf4404358849a
VikaDev <devm19102003@gmail.com>
2022-07-06 00:13:41 +0530

    ADD : Anime Detection

---
+++ cogs/anime_detection.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+from helpers import anime_detection_helper
+
+class AnimeDetection(commands.Cog):
+
+    @commands.command(name="detect-anime", aliases=["da"], description="Returns the anime name from the provided image.")
+    async def detect_anime(self, ctx:commands.Context, url):
+
+        embd = await anime_detection_helper.get_embed(url)
+
+        await ctx.reply(embed=embd)
+
+def setup(bot:commands.Bot):
+    bot.add_cog(AnimeDetection())
