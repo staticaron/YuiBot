0b96db5fa45156bd4b0e5aa9614738d40ad237c5
VikaDev <devm19102003@gmail.com>
2022-07-09 17:27:41 +0530

    ADD : Waifu Profile Pictures

--- helpers/misc_helper.py
+++ helpers/misc_helper.py
@@ -1,5 +1,15 @@
-from discord import Embed
+from discord import Embed, Member
 import requests
+
+async def fetch_waifu_api(endpoint:str) -> str:
+    
+    url = "https://waifu.pics/api/sfw/{}".format(endpoint)
+    waifu_resp = requests.get(url).json()
+
+    try:
+        return waifu_resp["url"]
+    except:
+        return None
 
 async def get_random_anime_quote_embed() -> Embed:
 
@@ -28,5 +38,16 @@ async def get_random_anime_quote_embed() -> Embed:
         value=quote_resp["quote"],
         inline=False
     )
+
+    return embd
+
+async def get_waifu_embed():
+
+    embd = Embed(
+        title="Waifu Picture"
+    )
+
+    url = await fetch_waifu_api("waifu")
+    embd.set_image(url=url)
 
     return embd
