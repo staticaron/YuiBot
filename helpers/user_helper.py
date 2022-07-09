b178d9bf4f6731dbd2c5c0b219b42d2a86da8286
VikaDev <devm19102003@gmail.com>
2022-07-09 15:37:51 +0530

    Pause

--- helpers/user_helper.py
+++ helpers/user_helper.py
@@ -132,6 +132,8 @@ async def get_user_embed(userID:str):
 
 async def follow_user(user:Member, target:Member) -> str:
 
+    print(type(target))
+
     # Get the target's anilist ID
     target_data = await mongo_manager.manager.get_user(str(target.id))
 
