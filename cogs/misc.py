fee394fdd15a45d4ebee137a9995d132cefb946c
VikaDev <devm19102003@gmail.com>
2022-07-09 17:27:41 +0530

    ADD : Waifu Profile Pictures

--- cogs/misc.py
+++ cogs/misc.py
@@ -11,5 +11,12 @@ class MiscModule(commands.Cog):
 
         await ctx.send(embed=reply)
 
+    @commands.command(name="pfp", aliases=["profile_pic", "picture", "waifu"], description="Returns a random waifu.")
+    @commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
+    async def waifu(self, ctx:commands.Context):
+        reply = await misc_helper.get_waifu_embed()
+
+        await ctx.send(embed=reply)
+
 def setup(bot):
     bot.add_cog(MiscModule())
