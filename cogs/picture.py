7775d217a9e1ca2efc056cd0f7cfb6876cb85a18
VikaDev <devm19102003@gmail.com>
2022-07-10 00:48:44 +0530

    ADD : Reaction gifs

--- cogs/picture.py
+++ cogs/picture.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import Member
 
 from helpers import picture_helper
 
@@ -11,5 +12,11 @@ class PictureModule(commands.Cog):
 
         await ctx.send(embed=reply)
 
+    @commands.command(name="gif", aliases=["react"], description="Returns a cry/sob gif")
+    async def cry(self, ctx:commands.Context, reaction:str, target:Member=None):
+        reply = await picture_helper.get_reaction_embed(reaction, ctx.author, target)
+
+        await ctx.send(embed=reply)
+
 def setup(bot):
-    bot.add_cog(PictureModule())
+    bot.add_cog(PictureModule(bot))
