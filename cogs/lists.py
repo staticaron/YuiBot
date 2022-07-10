6d06ce4c379b7d902b91d21e9ef6fb8b6b9a2d38
VikaDev <devm19102003@gmail.com>
2022-07-10 22:33:38 +0530

    ADD : Anime Selection Screen with a way to add them to list

--- cogs/lists.py
+++ cogs/lists.py
@@ -1,23 +1,44 @@
 from discord.ext import commands
+from discord.ui import Button
 
 from helpers import general_helper, lists_helper
 import config
 
 class ListsModule(commands.Cog):
 
-    @commands.group(name="add")
-    async def add_group(self, ctx:commands.Context):
-        if ctx.subcommand_passed is None:
-            await ctx.reply("Please provide a valid subcommand {}".format(config.YUI_SHY_EMOTE))
-            return
-
-    @add_group.command(name="ptw", aliases=["planning"], case_insensitive=True, description="Adds anime to your planning to watch list")
+    @commands.command(name="add", case_insensitive=True, description="Adds anime to your mentioned list")
     @commands.check(general_helper.validate_user)
-    async def ptw(self,ctx:commands.Context, *anime):
+    async def add(self,ctx:commands.Context, list_name:str, *anime):
+
+        # check for valid list
+        if list_name.lower() not in lists_helper.all_lists:
+            return await ctx.reply(embed=await general_helper.get_information_embed(
+                title="Hold It",
+                description="The following error occurred : ```INVALID LIST NAME``` Provide one of these list names : Planning, Dropped, Watching, Completed",
+                color=config.ERROR_COLOR
+            )
+        )
+
         anime = " ".join(anime)
 
-        await lists_helper.add_ptw(anime)
-        
+        async def select_callback(self):
+
+            mediaId = ids[paginator.current_page]
+
+            #modify the ptw list
+            reply = await lists_helper.add_to_list(list_name, mediaId, ctx.author)
+
+            paginator.clear_items()
+
+            await ctx.reply(embed=reply)
+
+            
+        response = await lists_helper.get_anime_selection_paginator(anime=anime, select_callback=select_callback)
+
+        paginator = response.paginator
+        ids = response.ids
+
+        await paginator.send(ctx)
 
 def setup(bot:commands.Bot):
     bot.add_cog(ListsModule())
--- helpers/lists_helper.py
+++ helpers/lists_helper.py
@@ -1,8 +1,38 @@
 import requests
+from discord import Embed, Member
 
+from views.select_view import SelectPaginator
+from managers import mongo_manager
+from helpers import general_helper
 import config
 
-async def add_ptw(anime:str):
+all_lists = ["ptw", "planning", "crt", "current", "watching", "comp", "completed", "drp", "dropped"]
+
+lists = {
+    "ptw" : "PLANNING",
+    "planning" : "PLANNING",
+    "crt" : "CURRENT",
+    "current" : "CURRENT",
+    "watching" : "CURRENT",
+    "comp" : "COMPLETED",
+    "completed" : "COMPLETED",
+    "drp" : "DROPPED",
+    "dropped" : "DROPPED"
+}
+
+"""Just a way to transmit paginator and ids at the same time. Please no bully"""
+
+class AnimePaginator:
+    paginator:SelectPaginator = None
+    ids:list = None
+
+    def __init__(self, paginator, ids):
+        self.paginator = paginator
+        self.ids = ids
+
+"""Returns a list of anime to select from"""
+
+async def get_anime_selection_paginator(anime:str, select_callback:callable) -> AnimePaginator:
 
     anime_query = """
         query($search:String){
@@ -16,6 +46,7 @@ async def add_ptw(anime:str):
                         english
                         romaji
                     }
+                    siteUrl
                     genres
                     coverImage{
                         medium
@@ -37,6 +68,74 @@ async def add_ptw(anime:str):
             "query" : anime_query,
             "variables" : variables
         }
-    )
+    ).json()
 
-    print(anime_resp.json())
+    anime_data = anime_resp["data"]["Page"]["media"]
+
+    pages = []
+    ids = []
+
+    for page_data in anime_data:
+        embd = Embed(
+            title=(page_data["title"]["english"] if page_data["title"]["english"] is not None else page_data["title"]["romaji"]),
+            color=config.NORMAL_COLOR,
+            url=page_data["siteUrl"]
+        )
+
+        embd.description = "**Genre** : {genre}\n**Episodes** : {episodes}\n**Status** : {status}".format(
+            genre="\n" + "\n".join(["{bullet}**{genre}**".format(bullet=config.BULLET_EMOTE, genre=genre) for genre in page_data["genres"]]),
+            episodes=page_data["episodes"],
+            status=page_data["status"]
+        )
+
+        embd.set_thumbnail(url=page_data["coverImage"]["medium"])
+
+        pages.append(embd)
+        ids.append(page_data["id"])
+
+    paginator = SelectPaginator(pages, select_callback)
+
+    return AnimePaginator(paginator, ids)
+
+"""Adds specified anime to the specified list"""
+
+async def add_to_list(list_name:str, mediaID:int, user:Member) -> Embed:
+
+    lst = lists[list_name]
+
+    list_query = """
+        mutation($id:Int!, $list:MediaListStatus){
+            SaveMediaListEntry(mediaId:$id, status:$list){
+                id 
+                status
+            }
+        }
+    """
+
+    token = (await mongo_manager.manager.get_user(str(user.id)))["token"]
+
+    list_resp = requests.post(
+        url=config.ANILIST_BASE,
+        json={
+            "query" : list_query,
+            "variables" : {
+                "id" : mediaID,
+                "list" : lst
+            }
+        },
+        headers={
+            "Authorization" : token
+        }
+    ).json()    
+    
+    if list_resp["data"]["SaveMediaListEntry"] is None:
+        return await general_helper.get_information_embed(
+            title="Whoops",
+            description="The following error occurred : ```{}```".format(list_resp["error"]["message"]),
+            color=config.ERROR_COLOR
+        )
+
+    return await general_helper.get_information_embed(
+        title="Done",
+        description="Anime was added to your `{}` list.".format(lst)
+    )
