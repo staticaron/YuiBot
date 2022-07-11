f6ac492bf9c077e9ec347c0b55b1dd47088e3d45
VikaDev <devm19102003@gmail.com>
2022-07-11 17:06:51 +0530

    ADD : AniList Logout

--- cogs/auth.py
+++ cogs/auth.py
@@ -6,6 +6,7 @@ from views.confirmation_view import ConfirmationView
 from managers import mongo_manager
 from helpers import general_helper, auth_helper
 from managers import mongo_manager
+from helpers import general_helper, auth_helper
 from config import ANILIST_LOGIN, ANILIST_ID, BULLET_EMOTE, LOADING_EMOTE, YUI_SHY_EMOTE
 
 class AuthModule(commands.Cog):
