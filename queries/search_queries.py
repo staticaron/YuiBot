6a81323814e3df77d06d897699e30e1fc9a76f0c
VikaDev <devm19102003@gmail.com>
2022-08-15 19:08:48 +0530

    ADD : Information footer to character embeds

--- queries/search_queries.py
+++ queries/search_queries.py
@@ -103,7 +103,39 @@ manga_query = """
 """
 
 character_query = """
+    query($search : String){
+        Character(search :$search, sort:SEARCH_MATCH){
+            id
+            name{
+                full
+                native
+                alternative
+            }
+            image{
+                medium
+            }
+            description(asHtml : false)
+            gender
+            dateOfBirth{
+                day
+                month
+                year
+            }
+            media(sort:POPULARITY_DESC){
+                nodes{
+                    title{
+                        english
+                        romaji
+                    }
+                    siteUrl
+                }
+            }
+            age
+            bloodType
+            siteUrl
+            favourites
 
-
-
+            isFavourite
+        }
+    }
 """
