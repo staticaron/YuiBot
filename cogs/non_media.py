0498387fb7377074b3b900bf4667202537905a97
VikaDev <devm19102003@gmail.com>
2022-08-19 10:27:12 +0530

    FIX : Indentation

--- cogs/non_media.py
+++ cogs/non_media.py
@@ -3,20 +3,21 @@ from discord.ext import pages
 
 from helpers import general_helper, non_media_helper
 
+
 class Non_Media_Module(commands.Cog):
-    
+
     @commands.group(name="character", aliases=["char"], description="Commands related to managing characters in your anilist", case_insensitive=True)
-    async def character(self, ctx:commands.Context):
-        if ctx.subcommand_passed ==  None:
+    async def character(self, ctx: commands.Context):
+        if ctx.subcommand_passed == None:
             await ctx.reply("Please provide valid subcommand. Try ```yui help character```")
 
     @character.command(name="fav", description="Returns your favorite characters if no param is provided else adds the provided character to favorites", case_insensitive=True)
-    async def favorite(self, ctx:commands.Context, *character):
+    async def favorite(self, ctx: commands.Context, *character):
 
         await ctx.trigger_typing()
 
         if len(character) <= 0:
-            
+
             fav_scroller = await non_media_helper.get_fav_character_scroller(ctx.author)
 
             return await fav_scroller.send(ctx)
@@ -30,13 +31,15 @@ class Non_Media_Module(commands.Cog):
 
         response = await general_helper.get_character_selection_paginator(character, character_selection_callback)
 
-        paginator:pages.Paginator = response.paginator
-        ids = [str(data_element.anilist_id) for data_element in response.data_elements]
+        paginator: pages.Paginator = response.paginator
+        ids = [str(data_element.anilist_id)
+               for data_element in response.data_elements]
 
         if response.length() > 0:
             await paginator.send(ctx)
         else:
             await ctx.reply(embed=await response.get_error_embed())
 
-def setup(bot:commands.Bot):
+
+def setup(bot: commands.Bot):
     bot.add_cog(Non_Media_Module())
