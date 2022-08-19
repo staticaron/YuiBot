19ecb2ea011b54495e6f39715ed322a9e9fa9750
VikaDev <devm19102003@gmail.com>
2022-08-19 09:14:38 +0530

    ADD : Error Notifier

--- cogs/error_handler.py
+++ cogs/error_handler.py
@@ -1,15 +1,17 @@
 from discord.ext import commands
+from discord import Embed
 import traceback
 import sys
 
 from helpers import general_helper
 import config
 
+
 class ErrorHandlerModule(commands.Cog):
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx:commands.Context, error):
-        
+    async def on_command_error(self, ctx: commands.Context, error):
+
         cog = ctx.cog
 
         if hasattr(ctx.command, "on_error"):
@@ -37,7 +39,7 @@ class ErrorHandlerModule(commands.Cog):
         elif isinstance(error, commands.CommandOnCooldown):
             time = int(ctx.command.get_cooldown_retry_after(ctx))
             time_str = await general_helper.get_time_str_from_seconds(time)
-            await ctx.reply("You are on a cooldown. Please wait for {}".format(time_str))  
+            await ctx.reply("You are on a cooldown. Please wait for {}".format(time_str))
 
         elif isinstance(error, commands.CheckFailure):
             await ctx.reply(embed=await general_helper.get_information_embed(
@@ -47,8 +49,20 @@ class ErrorHandlerModule(commands.Cog):
             ))
 
         else:
-            print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            embd = Embed(title="Damn",
+                         color=config.ERROR_COLOR,
+                         description="Error occurred while trying to run this command.",)
+
+            embd.add_field(
+                name="Details for Nerds",
+                value="```{}```".format(error),
+                inline=False
+            )
+
+            await ctx.reply(embed=embd)
+
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr)
 
 
 def setup(bot):
