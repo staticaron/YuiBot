5817f9d153c0e668a6478a713daf541bfb4b307c
staticaron <devm19102003@gmail.com>
2025-05-15 01:13:40 +0530

    ADD : Alternative Song Detection

--- helpers/spotify_helper.py
+++ helpers/spotify_helper.py
@@ -1,54 +1,91 @@
 from discord import Message
-import aiospotify
 import pyyoutube
 import ytmusicapi
-
 import config
+import asyncio
+import spotipy
+
 
 class SpotifyTrackAlternative:
-    youtube_video:str = None
-    youtube_music:str = None
-    apple_music:str = None
-    amazon_music:str = None
+    track_name: str = None
 
-async def youtube_fetch(track_name:str, track_artists:str):
+    track_artists: str = None
+
+    youtube_video: str = None
+
+    youtube_music: str = None
+
+    apple_music: str = None
+
+    amazon_music: str = None
+
+
+async def youtube_fetch(track_name: str, track_artists: str):
+    """Fetch youtube video based on track_name and track_artists"""
+
     youtube_client = pyyoutube.Api(api_key=config.YOUTUBE_API_KEY)
-    youtube_track = youtube_client.search_by_keywords(q="{} {}".format(track_name, track_artists), search_type=["video"], count=1, limit=1)
+
+    youtube_track = youtube_client.search_by_keywords(
+        q="{} {}".format(track_name, track_artists),
+        search_type=["video"],
+        count=1,
+        limit=1,
+    )
+
     return youtube_track
 
-async def youtube_music_fetch(track_name:str, track_artists:str):
+
+async def youtube_music_fetch(track_name: str, track_artists: str):
+    """Fetch music video from youtube music based on track name and artist name"""
+
     youtube_music_client = ytmusicapi.YTMusic()
+
     result = youtube_music_client.search("{} {}".format(track_name, track_artists))
     return result
 
-async def fetch_spotify_track(track_id):
-    spotify_async_client:aiospotify.Aiospotify = aiospotify.Aiospotify(client_id=config.SPOTIFY_CLIENT_ID, client_secret=config.SPOTIFY_CLIENT_SECRET)
-    spotify_auth_token:str = await spotify_async_client.init_auth()
-    return spotify_async_client.search_track(id_=track_id, token=spotify_auth_token)
 
-async def send_alternatives(message:Message, spotify_track_id:str) -> SpotifyTrackAlternative:
+def fetch_spotipy(track_id):
+    """Fetch the track details from spotify"""
 
-    links:SpotifyTrackAlternative = SpotifyTrackAlternative()
+    auth_manager = spotipy.SpotifyClientCredentials(
+        client_id=config.SPOTIFY_CLIENT_ID, client_secret=config.SPOTIFY_CLIENT_SECRET
+    )
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+    return sp.track(track_id)
 
-    # spotify_credentials:spotipy.SpotifyClientCredentials = spotipy.SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET)
-    # spotify_client:spotipy.Spotify = spotipy.Spotify(auth_manager=spotify_credentials)
 
-    spotify_track = await fetch_spotify_track(spotify_track_id)
+async def get_alternatives(
+    message: Message, spotify_track_id: str
+) -> SpotifyTrackAlternative:
+    """Get alternative links to the current spotify song"""
 
-    all_artists:list[str] = []
+    links: SpotifyTrackAlternative = SpotifyTrackAlternative()
+
+    spotify_track = await asyncio.to_thread(fetch_spotipy, spotify_track_id)
+
+    all_artists: list[str] = []
+
     for artist_details in spotify_track.get("artists"):
         all_artists.append(artist_details.get("name"))
-    
-    track_name:str = spotify_track.get("name")
-    track_artists:str = " - ".join(all_artists)
 
-    video_results = await youtube_fetch(track_name, track_artists)
-    music_results = await youtube_music_fetch(track_name, track_artists)
+    links.track_name = spotify_track.get("name")
 
-    links.youtube_video = config.YOUTUBE_TRACK_BASE + video_results.items[0].id.videoId if len(video_results.items) > 0 else None
-    links.youtube_music = config.YOUTUBE_MUSIC_BASE + music_results[0].get("videoId") if len(music_results) > 0 else None
-    
+    links.track_artists = " - ".join(all_artists)
+
+    video_results = await youtube_fetch(links.track_name, links.track_artists)
+
+    music_results = await youtube_music_fetch(links.track_name, links.track_artists)
+
+    links.youtube_video = (
+        config.YOUTUBE_TRACK_BASE + video_results.items[0].id.videoId
+        if len(video_results.items) > 0
+        else None
+    )
+
+    links.youtube_music = (
+        config.YOUTUBE_MUSIC_BASE + music_results[0].get("videoId")
+        if len(music_results) > 0
+        else None
+    )
+
     return links
-
-
-    
--- utils/bot.py
+++ utils/bot.py
@@ -34,26 +34,50 @@ class Bot(commands.Bot):
         print("Discord Version : {}".format(__version__))
 
     async def on_message(self, message: Message):
-
         if message.content.strip() in self.mentions:
-
-            embd = Embed(title="Ya-Ho :wave:",
-                         description=f"Prefix : **yui**\nLatency : **{round(self.latency * 1000, 2)} ms**\nInvite : [Click Here]({config.INVITE})", color=config.NORMAL_COLOR).set_thumbnail(url=self.user.avatar.url)
+            embd = Embed(
+                title="Ya-Ho :wave:",
+                description=f"Prefix : **yui**\nLatency : **{round(self.latency * 1000, 2)} ms**\nInvite : [Click Here]({config.INVITE})",
+                color=config.NORMAL_COLOR,
+            ).set_thumbnail(url=self.user.avatar.url)
 
             return await message.channel.send(embed=embd)
 
         SPOTIFY_TRACK_BASE = "https://open.spotify.com/track/"
 
         if message.content.strip().startswith(SPOTIFY_TRACK_BASE):
-
             splits = message.content.strip().split()
-            track_id_match = re.findall(r"(?<=track/)\w+", splits[0]) if len(splits) > 0 else None
-            
-            if len(track_id_match) > 0:
-                links = await spotify_helper.send_alternatives(message=message, spotify_track_id=track_id_match[0])
-                embd:Embed = await general_helper.get_information_embed(title="Alternate Links", description="Best Matches")
-                view = SpotifyView(links)
+            track_id_match = (
+                re.findall(r"(?<=track/)\w+", splits[0]) if len(splits) > 0 else None
+            )
 
-                await message.reply(embed=embd, view=view)
+            if len(track_id_match) > 0:
+                links: spotify_helper.SpotifyTrackAlternative = (
+                    await spotify_helper.get_alternatives(
+                        message=message, spotify_track_id=track_id_match[0]
+                    )
+                )
+
+                EMBD_ENABLED = False
+
+                if EMBD_ENABLED:
+                    embd: Embed = await general_helper.get_information_embed(
+                        title="Alternate Links", description=""
+                    )
+                    embd.description += "**Name : **" + links.track_name
+                    embd.description += "\n**Artists: **" + links.track_artists
+                    view = SpotifyView(links)
+
+                    await message.reply(embed=embd, view=view)
+                else:
+                    message = await message.reply(
+                        "**Name :** {}, **Artists :** {} | [Youtube Music]({})".format(
+                            links.track_name,
+                            links.track_artists,
+                            links.youtube_music,
+                        )
+                    )
+
+                    await message.edit(suppress=True)
 
         await self.process_commands(message)
